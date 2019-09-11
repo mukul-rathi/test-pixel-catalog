531b984668219bd3898827cf2c3fc2e325cb291b
Mukul Rathi <mukul.rathi@gmail.com>
2019-09-11 15:04:50 +0100

    Update OG Metadata

--- index.php
+++ index.php
@@ -33,72 +33,34 @@
 
     <!-- Open Graph Metadata -->
   <meta property="og:type" r="website">
-  <meta id="title" property="og:title" content="<?php echo_param_or_default("title", "some product"); ?>">
+  <meta property="product:retailer_item_id" content="<?php echo_param_or_default("retailer_item_id", "id_1"); ?>">
   <meta property="og:url" content="<?php echo "https://test-pixel-catalog.herokuapp.com".$_SERVER['REQUEST_URI']; ?>">
+  <meta property="og:title" content="<?php echo_param_or_default("title", "some product"); ?>">
   <meta property="og:description" content="<?php echo_param_or_default("description", "some description"); ?>">
   <meta property="og:image" content="<?php echo_param_or_default("image_url", "https://example.org/image.jpg"); ?>">
   <meta property="og:site_name" content="Mukul's test catalog">
-  <meta property="product:brand" content="<?php echo_param_or_default("title", "some product"); ?>">
+  <meta property="product:brand" content="<?php echo_param_or_default("brand", "some brand"); ?>">
+  <meta property="product:gender" content="<?php echo_param_or_default("gender", "unisex"); ?>">
   <meta property="product:category" content="<?php echo_param_or_default("category", "Tech Supplies"); ?>">
   <meta property="product:availability" content="<?php echo_param_or_default("availability", "in stock"); ?>">
-  <meta id="inventory" property="product:inventory" content="<?php echo_param_or_default("inventory",50); ?>">
+  <meta property="product:inventory" content="<?php echo_param_or_default("inventory",50); ?>">
   <meta property="product:condition" content="<?php echo_param_or_default("condition", "new"); ?>">
-  <meta id="amount"  property="product:price:amount" content="<?php echo_param_or_default("price_amount", "99"); ?>">
-  <meta id="currency"  property="product:price:currency" content="<?php echo_param_or_default("price_currency", "GBP"); ?>">
-  <meta id="prod_id" property="product:retailer_item_id" content="<?php echo_param_or_default("retailer_item_id", "id_1"); ?>">
+  <meta property="product:price:amount" content="<?php echo_param_or_default("price_amount", "99"); ?>">
+  <meta property="product:price:currency" content="<?php echo_param_or_default("price_currency", "GBP"); ?>">
+  <meta property="product:custom_label_1" content="<?php echo_param_or_default("retailer_item_id", "flag_1"); ?>">
+  <meta property="product:custom_label_0" content="<?php echo_param_or_default("retailer_item_id", "flag_0"); ?>">
   <!-- End Open Graph Metadata -->
-
 </head>
 <body>
-  <h1>
-      <script> document.getElementById('title').getAttribute('content') </script>
-  </h1>
   <ul>
       <script>
   for(let metaTag of document.getElementsByTagName("meta")){
     let li = document.createElement('li');
     li.innerHTML =  metaTag.getAttribute('property') + ": " + metaTag.getAttribute('content');
     document.body.appendChild(li);
-
   };
 </script>
   </ul>
-  <button type="button" id="addToCartButton">Test add to cart!</button>
-  <button type="button" id="purchaseButton">Test purchase!</button>
-
-
-  <script>
-  const prodId = document.getElementById('prod_id').getAttribute('content');
-    fbq('track', 'ViewContent', {
-      content_type: 'product',
-      content_id: prodId
-
-    });
-
-    document.getElementById('addToCartButton').addEventListener('click', function(e) {
-      e.preventDefault();
-      const prodId = document.getElementById('prod_id').getAttribute('content');
-      console.log("add to cart");
-      fbq('track', 'AddToCart', {
-        content_type: 'product',
-        content_id: prodId
-      });
-}, false);
-document.getElementById('purchaseButton').addEventListener('click', function(e) {
-  e.preventDefault();
-  const val = document.getElementById('amount').getAttribute('content');
-  const cur = document.getElementById('currency').getAttribute('content');
-  const prodId = document.getElementById('prod_id').getAttribute('content');
-  console.log("purchase");
-  fbq('track','Purchase', {
-    content_type: 'product',
-    value: val,
-    currency: cur,
-    content_id: prodId
-
-  });
-}, false);
-  </script>
   <div itemscope itemtype="">
      <div itemscope itemtype="http://schema.org/Product">
        <span itemprop="name"><?php echo_param_or_default("title", "some product"); ?></span>
